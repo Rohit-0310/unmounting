0d2b72063da354066cc1a17647944e77cdbc54e9
ROHIT KUMAR <rohit9909mr@gmail.com>
2021-12-30 16:45:05 +0530

    Timer Stop

--- src/components/Timer.jsx
+++ src/components/Timer.jsx
@@ -5,7 +5,12 @@ export const Timer = () => {
     useEffect(() => {
         setInterval(() => {
             // setCounter(counter - 1);
-            setCounter((p) => p + 1);
+            setCounter((p) => {
+                if(p === 0) {
+                    return 0;
+                }
+                return p-1;
+            })
         }, 1000);
     }, []);
     return <li>Counter is : {counter} </li>
