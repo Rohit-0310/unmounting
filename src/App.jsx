9a14f13c9993380fc15855e234e98109b791ab57
ROHIT KUMAR <rohit9909mr@gmail.com>
2021-12-30 16:34:46 +0530

    Timer Working

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { Timer } from './components/Timer';
 import { Todo } from './components/Todo';
 
 function App() {
+    const [count, setCount] = useState(0)
   return (
     <div className="App">
+        <h3>Count is: {count}</h3>
+        <button onClick={() =>setCount(count + 1)}>Add by 1</button>
       <Todo />
+      <Timer />
     </div>
   );
 }
